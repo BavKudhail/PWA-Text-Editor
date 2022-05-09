adeb4b36e603855c99cd476b1d2dbd41b14dcdd3
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-09 20:57:21 +0100

    updated sw.js file

--- client/src-sw.js
+++ client/src-sw.js
@@ -24,7 +24,10 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-({ request }) => ["style", "script", "worker"].includes(request.destination),
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: "asset-cache",
     plugins: [
@@ -32,7 +35,5 @@ warmStrategyCache({
         statuses: [0, 200],
       }),
     ],
-  });
-
-// TODO: Implement asset caching
-registerRoute();
+  })
+);
